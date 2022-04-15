e9bcfbc99a2dbc38b7054e210a39e6db3126e05b
USU-Brandon <92476389+USU-Brandon@users.noreply.github.com>
2022-04-15 10:01:33 -0600

    Programmed in some interaction behaviors

--- Bio-Aggression/Bio-Aggression.cpp
+++ Bio-Aggression/Bio-Aggression.cpp
@@ -6,6 +6,29 @@
 #define MCW MPI_COMM_WORLD
 using namespace std;
 
+// 20 percent chance to attack another Beta. Increased likelihood with higher standing.
+bool betaInteraction(int rank, int standing) {
+    int interactionChance = ((rand() + rank) % 10) + ceil(standing / 2);
+    if (interactionChance >= 8) { return true; }
+    else { return false; }
+}
+
+//only 1/7 chance to interact with female. Decreases further with lower standing.
+bool femaleInteraction(int rank, int standing) {
+    int interactionChance = ((rand() + rank) % 21) - ceil(5/standing);
+    if (interactionChance >= 18) { return true; }
+    else { return false; }
+
+}
+
+// Generate attack value from 1 - 50. The higher the standing, the greater the advantage.
+// Alpha (low, high) = (25, 74)     Beta 0 = (1,51)
+int generateAttack(int rank, int standing) {
+    int power = ((rand() + rank) % 50) + pow(standing, 2);
+    return power;
+}
+
+
 int main(int argc, char** argv)
 {
     int rank, size;
@@ -14,6 +37,18 @@ int main(int argc, char** argv)
     MPI_Comm_rank(MCW, &rank);
     MPI_Comm_size(MCW, &size);
     srand(time(0) + rank);
+
+    // The female process
+    if (!rank) {
+
+    }
+
+    // Male processes
+    if (rank) {
+        int standing = 0;
+        bool isAlpha = false;
+
+    }
 
     MPI_Finalize();
     return 0;
